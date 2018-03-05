7e892c76614a96fb2dac7fe0ead2d22eb44c8861
Kelvin Louis <me@kelvinlouis.com>
2018-03-05 10:56:43 +0100

    Assignment 3: Doubly Linked List with prev and next iterations for better performance (WIP)

--- ch.fhnw.algd2.iterators/src/ch/fhnw/algd2/collections/list/linkedlist/DoublyLinkedList.java
+++ ch.fhnw.algd2.iterators/src/ch/fhnw/algd2/collections/list/linkedlist/DoublyLinkedList.java
@@ -3,6 +3,7 @@ package ch.fhnw.algd2.collections.list.linkedlist;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.ListIterator;
+import java.util.Objects;
 
 import ch.fhnw.algd2.collections.list.MyAbstractList;
 
@@ -13,7 +14,18 @@ public class DoublyLinkedList<E> extends MyAbstractList<E> {
 
 	@Override
 	public boolean add(E e) {
-		throw new UnsupportedOperationException();
+		Node<E> newNode = new Node(last, e, null);
+
+		if (first == null) {
+			first = newNode;
+			last = first;
+		} else {
+			last = newNode;
+		}
+
+		++size;
+		increaseModCount();
+		return true;
 	}
 
 	@Override
@@ -23,22 +35,185 @@ public class DoublyLinkedList<E> extends MyAbstractList<E> {
 
 	@Override
 	public boolean remove(Object o) {
-		throw new UnsupportedOperationException();
+		Objects.requireNonNull(o);
+
+		// Check from the start and end (faster search)
+		Node<E> startNode = first;
+		Node<E> endNode = last;
+
+		if (startNode == endNode && startNode != null) {
+			if (startNode.elem.equals(o)) {
+				removeHead();
+				return true;
+			}
+		}
+
+		while (startNode != null && endNode != startNode) {
+			if (startNode.elem.equals(o)) {
+				return removeNode(startNode);
+			}
+			if (endNode.elem.equals(o)) {
+				return removeNode(startNode);
+			}
+
+			startNode = startNode.next;
+			endNode = endNode.prev;
+		}
+		return false;
+	}
+
+	private boolean removeNode(Node<E> node) {
+		Node<E> prev = node.prev;
+		Node<E> next = node.next;
+
+		prev.next = next;
+		next.prev = prev;
+
+		node.next = null;
+		node.prev = null;
+
+		--size;
+		increaseModCount();
+
+		return true;
 	}
 
 	@Override
 	public E remove(int index) {
-		throw new UnsupportedOperationException();
+		Objects.requireNonNull(index);
+		if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+
+		int half = size / 2;
+
+		if (index == 0) {
+			return removeHead();
+		} else if (index == size-1) {
+			return removeTail();
+		} else {
+			if (index <= half) {
+				return removeAtUsingNext(index);
+			} else {
+				return removeAtUsingPrev(index);
+			}
+		}
+	}
+
+	private E removeHead() {
+		Node<E> removedNode = first;
+
+		first = first.next;
+
+		if (first != null) {
+			first.prev = null;
+		}
+
+		--size;
+
+		if (size == 0 || size == 1) {
+			last = first;
+		}
+
+		increaseModCount();
+		removedNode.next = null;
+		return removedNode.elem;
+	}
+
+	private E removeTail() {
+		Node<E> removedNode = last;
+
+		last = removedNode.prev;
+		last.next = null;
+		--size;
+
+		if (size == 1) {
+			first = last;
+		}
+
+		increaseModCount();
+		return removedNode.elem;
+	}
+
+	private E removeAtUsingNext(int index) {
+		Node<E> node = first;
+		int i = 0;
+
+		while(i < index) {
+			node = node.next;
+			i++;
+		}
+
+		removeNode(node);
+		return node.elem;
+	}
+
+	private E removeAtUsingPrev(int index) {
+		Node<E> node = first;
+		int i = size - 1;
+
+		while(i > index) {
+			node = node.prev;
+			i--;
+		}
+
+		removeNode(node);
+		return node.elem;
 	}
 
 	@Override
 	public boolean contains(Object o) {
-		throw new UnsupportedOperationException();
+		Objects.requireNonNull(o);
+
+		// Check from the start and end (faster search)
+		Node<E> startNode = first;
+		Node<E> endNode = last;
+
+		if (startNode == endNode && startNode != null) {
+			// First and last are same (just one element)
+			return startNode.elem.equals(o);
+		}
+
+		while (startNode != null && endNode != startNode) {
+			if (startNode.elem.equals(o)) {
+				return true;
+			}
+			if (endNode.elem.equals(o)) {
+				return true;
+			}
+
+			startNode = startNode.next;
+			endNode = endNode.prev;
+		}
+		return false;
 	}
 
 	@Override
 	public E get(int index) {
-		throw new UnsupportedOperationException();
+		Objects.requireNonNull(index);
+		if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+
+		int half = size / 2;
+
+		if (index <= half) {
+			return getByNext(index);
+		} else {
+			return getByPrev(index);
+		}
+	}
+
+	private E getByNext(int index) {
+		Node<E> node = first;
+		for(int i = 0; i < index; i++) node = node.next;
+		return node.elem;
+	}
+
+	private E getByPrev(int index) {
+		Node<E> node = last;
+		for(int i = size - 1; i > index; i--) node = node.prev;
+		return node.elem;
+	}
+
+	private void increaseModCount() {
+		modCount++;
 	}
 
 	@Override
